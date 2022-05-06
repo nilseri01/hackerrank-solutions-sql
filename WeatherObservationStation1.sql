cedce72b384e11807bdc0f2dbf872c6b5f5f40e9
Nil Seri <senoritadeveloper01@gmail.com>
2022-05-06 18:53:35 +0300

    Create WeatherObservationStation1.sql

---
+++ WeatherObservationStation1.sql
@@ -0,0 +1,6 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-1
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select s.city, s.state from station s;
