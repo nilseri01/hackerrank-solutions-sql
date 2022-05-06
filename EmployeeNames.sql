3c0b73e71773daae2c1e5f4fa0c483ae9e881d47
Nil Seri <senoritadeveloper01@gmail.com>
2022-05-06 20:17:08 +0300

    Create EmployeeNames.sql

---
+++ EmployeeNames.sql
@@ -0,0 +1,7 @@
+-- https://www.hackerrank.com/challenges/name-of-employees/problem
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select e.name from employee e order by e.name asc;
