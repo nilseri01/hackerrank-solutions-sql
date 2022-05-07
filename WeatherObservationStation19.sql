a17ff47bb8636342dd50927a89e7efc2a00f4efd
Nil Seri <senoritadeveloper01@gmail.com>
2022-05-07 17:57:23 +0300

    Create WeatherObservationStation19.sql

---
+++ WeatherObservationStation19.sql
@@ -0,0 +1,8 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-19/problem
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select round(sqrt(power((max(lat_n) - min(lat_n)), 2) + power((max(long_w) - min(long_w)), 2)), 4)
+from station;
