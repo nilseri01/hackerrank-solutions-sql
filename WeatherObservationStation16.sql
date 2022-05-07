395ab6c5737bed6b4c3dc16a8ae381a3a577b5d5
Nil Seri <senoritadeveloper01@gmail.com>
2022-05-07 15:44:43 +0300

    Create WeatherObservationStation16.sql

---
+++ WeatherObservationStation16.sql
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-16
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select round(min(s.lat_n), 4)
+from station s
+where s.lat_n > 38.7780;
